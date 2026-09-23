53425e8d613b27d562cda5729f17d479eb02553f
Shilna <shilnakolangath416@gmail.com>
2026-09-23 13:18:43 +0530

    Time: 857 ms (70.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,6 @@
+select p.product_id,round(coalesce(sum(p.price*u.units) / nullif(sum(u.units),0),0),2) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
